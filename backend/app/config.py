3845ffa6d7a84a7935fb51551159e825c5c10a2f
Jakub Madry <jaiwiecejmnie@gmail.com>
2025-01-17 20:43:40 +0100

    Fix server config

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,5 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@localhost:5432/ariusDB'
-    #SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@postgresDB:5432/ariusDB'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://admin:admin@postgresDB:5432/ariusDB'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '6e48ff4302924803ba0df47c2e307c78'
     # JWT_SECRET_KEY = 'your-jwt-secret-key'
